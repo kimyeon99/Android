ef2a6e492f2fd2f5f01fd90b42831a80bc978b66
kimyeon99 <tmdwo7017@naver.com>
2021-06-01 20:52:01 +0900

    my part of team's app

---
+++ Board.java
@@ -0,0 +1,25 @@
+package com.example.yjtimes2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+// import android.support.v4.app.Fragment;
+
+public class Board extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.board_2, container, false);
+
+        return view;
+    }
+}
